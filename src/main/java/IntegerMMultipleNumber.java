b69375585171507f93839301439cb0195eeadf18
ViacheslavAnisimov <forvsn@gmail.com>
2023-02-15 17:20:33 +0700

    edit a test

--- src/main/java/IntegerMMultipleNumber.java
+++ src/main/java/IntegerMMultipleNumber.java
@@ -12,10 +12,10 @@ public class IntegerMMultipleNumber {
             if (m % 7 == 0 && m % 9 == 0) {
 
                 return "Good Number";
-            } else if (m % 7 == 0 && m % 9 != 0) {
+            } else if (m % 9 == 0 && m % 7 != 0) {
 
                 return "Bad Number";
-            } else if (m % 11 == 0 && m % 7 != 0 && m % 9 != 0) {
+            } else if (m % 11 == 0 && m % 9 != 0 && m % 7 != 0) {
 
                 return "Poor Number";
             }
--- src/test/java/IntegerMMultipleNumberTest.java
+++ src/test/java/IntegerMMultipleNumberTest.java
@@ -9,22 +9,22 @@ public class IntegerMMultipleNumberTest {
         assertEquals("Zero is always multiple of every number", new IntegerMMultipleNumber().integerMMultipleNumber(m));
     }
     @Test
-    public void integerMMultiple7and9Number() {
+    public void integerMMultipleNineAndNineNumber() {
         int m = 63;
         assertEquals("Good Number", new IntegerMMultipleNumber().integerMMultipleNumber(m));
     }
     @Test
-    public void integerMMultiple7andNotMultiple9Number() {
-        int m = 49;
+    public void integerMMultipleNineandNotMultipleSevenNumber() {
+        int m = 81;
         assertEquals("Bad Number", new IntegerMMultipleNumber().integerMMultipleNumber(m));
     }
     @Test
-    public void integerMMultiple11AndNotMultiple7and9Number() {
+    public void integerMMultipleElevenAndNotMultipleSevenAndNineNumber() {
         int m = 121;
         assertEquals("Poor Number", new IntegerMMultipleNumber().integerMMultipleNumber(m));
     }
     @Test
-    public void integerMNotMultiple11And7and9Number() {
+    public void integerMNotMultipleElevenAndSevenAndNineNumber() {
         int m = 10;
         assertEquals("-1", new IntegerMMultipleNumber().integerMMultipleNumber(m));
     }
