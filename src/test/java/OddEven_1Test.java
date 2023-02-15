0068e0104b2fc8891bdcaee98253a4ac03db6921
ViacheslavAnisimov <forvsn@gmail.com>
2023-02-15 09:02:45 +0700

    added new test

---
+++ src/test/java/OddEven_1Test.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OddEven_1Test {
+    OddEven OddEven = new OddEven();
+
+    @Test
+    public void oddEven1() {
+        assertEquals("Odd", OddEven.oddEven(-345));
+    }
+    @Test
+    public void oddEven2() {
+        assertEquals("Even", OddEven.oddEven(0));
+    }
+    @Test
+    public void oddEven3() {
+        assertEquals("Even", OddEven.oddEven(222222));
+    }
+}
