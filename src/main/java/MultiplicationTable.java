ddd357b60535b1dcba54907e2017f4068ad83455
ViacheslavAnisimov <forvsn@gmail.com>
2023-03-10 09:44:00 +0700

    edit unit

--- src/main/java/MultiplicationTable.java
+++ src/main/java/MultiplicationTable.java
@@ -1,8 +1,27 @@
+import java.util.Arrays;
+
 public class MultiplicationTable {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
                 System.out.print((i * j) + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println(
+
+        );
+        int[][] arr = new int[10][10];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (i + 1) * (j + 1);
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
             }
             System.out.println();
         }
